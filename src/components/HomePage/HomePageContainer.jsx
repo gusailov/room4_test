e2bfd0dd6bc0bed44a0ff6134f074fdc3f3b94f8
gusailov <gusailov@gmail.com>
2020-12-28 09:23:31 +0200

    home_pagination_cut

--- src/components/HomePage/HomePageContainer.jsx
+++ src/components/HomePage/HomePageContainer.jsx
@@ -2,9 +2,9 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from "react-redux";
 import { Grid, Typography, List } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-import { getTopTracks } from "../../redux/main_page-reduser";
+import { getTopTracks } from "../../redux/home_page-reduser";
 import { HomePageListItem } from './HomePageListItem';
-import Pagination from '@material-ui/lab/Pagination';
+
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -19,20 +19,18 @@ export const HomePageContainer = (props) => {
 
     const isFetching = useSelector(state => state.mainPage.isFetching)
     const mainPage = useSelector(state => state.mainPage)
-    const pages = Number(useSelector(state => state.mainPage.pages))
+
     const classes = useStyles();
     const dispatch = useDispatch()
-    const [page, setPage] = React.useState(1);
+
     const querry = props.title
     useEffect(() => {
         console.log('querry', querry);
-        dispatch(getTopTracks(querry, page));
-    }, [dispatch, page, querry]);
+        dispatch(getTopTracks(querry));
+    }, [dispatch, querry]);
     console.log('mainPage', mainPage);
     const tracks = useSelector(state => state.mainPage.tracks);
-    const handleChange = (event, value) => {
-        setPage(value);
-    };
+
 
     return (
 
@@ -41,7 +39,7 @@ export const HomePageContainer = (props) => {
                 <Typography gutterBottom variant="h5" component="h2">
                     {props.title}
                 </Typography>
-                <Pagination count={pages} page={page} onChange={handleChange} />
+
             </Grid>
             <Grid item>
                 <List className={classes.root}>
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,11 @@
 import { applyMiddleware, combineReducers, createStore } from "redux";
-import mainPageReducer from "./main_page-reduser";
+import homePageReducer from "./home_page-reduser";
 import artistPageReducer from "./artist_page-reduser";
 import searchPageReducer from "./search_page-reduser";
 import thunkMiddleware from "redux-thunk";
 
 const reducers = combineReducers({
-  mainPage: mainPageReducer,
+  mainPage: homePageReducer,
   artistPage: artistPageReducer,
   searchPage: searchPageReducer,
 });
